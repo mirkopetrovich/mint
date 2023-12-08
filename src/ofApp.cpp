d211e963b3a6c947617621d1bc885be3bee3708e
mirkopetrovich <mirkopetrovich@gmail.com>
2023-12-08 20:09:02 -0300

    players + box2d

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,30 +3,91 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     
-    ofDisableArbTex();
+    ofSetLogLevel(OF_LOG_NOTICE);
+    
+    ofSetFrameRate(60);
+    ofSetBackgroundAuto(true);
+    ofSetVerticalSync(true);
+    ofEnableAlphaBlending();
+    ofBackground(0);
+    
+    ofDisableArbTex(); //Use GL_TEXTURE_2D textures.
+    /// GL_TEXTURE_RECTANGLE textures are more intuitive since they allow pixel
+    /// based coordinates and are enabled by default.
+    ///
+    /// GL_TEXTURE_2D textures use normalised texture coordinates (a float value
+    /// between 0 and 1 is used to express texture coordinates along width and
+    /// height).
+    ///
+    /// GL_TEXTURE_2D textures are more widely supported and allow advanced features
+    /// such as mipmaps and texture compression.
+    
     shaderX.load("shaderBlurX");
     shaderY.load("shaderBlurY");
     fondo.load("fondo.jpg");
-    ofSetCircleResolution(50);
-    ofEnableAlphaBlending();
-    ofSetBackgroundAuto(true);
+
     
-    pos.set(860,206);
     
-    fbo1.allocate(1920,512,GL_RGBA);
-    fbo1.begin();
+    // --------------  box2d settings  ----------------------
+    box2d.init();
+    box2d.setGravity(0,-1.4);
+    //box2d.createGround(0,50, 1920,50);
+    //box2d.createBounds(0,0, 1921,513);
+    //box2d.checkBounds(true);
+    //box2d.setFPS(60.0);
+    box2d.registerGrabbing();
+    // -------------------------------------------------------
+    
+    
+    
+    //  -------------- framebuffer settings -----------------------
+    fb_player_1.allocate(1920,512,GL_RGBA);
+    fb_player_1.begin();
     ofClear(0,0,0,0);
-    fbo1.end();
+    fb_player_1.end();
     
-    fbo2.allocate(1920,512,GL_RGBA);
-    fbo2.begin();
+    fb_player_2.allocate(1920,512,GL_RGBA);
+    fb_player_2.begin();
     ofClear(0,0,0,0);
-    fbo2.end();
+    fb_player_2.end();
     
-    fbo3.allocate(1920,512,GL_RGBA);
-    fbo3.begin();
+    fb_player_3.allocate(1920,512,GL_RGBA);
+    fb_player_3.begin();
     ofClear(0,0,0,0);
-    fbo3.end();
+    fb_player_3.end();
+    
+    fb_mix.allocate(1920,512,GL_RGBA);
+    fb_mix.begin();
+    ofClear(0,0,0,0);
+    fb_mix.end();
+    
+    
+    fb_blur_X.allocate(1920,512,GL_RGBA);
+    fb_blur_X.begin();
+    ofClear(0,0,0,0);
+    fb_blur_X.end();
+    
+    fb_blur_Y.allocate(1920,512,GL_RGBA);
+    fb_blur_Y.begin();
+    ofClear(0,0,0,0);
+    fb_blur_Y.end();
+    // ---------------------------------------------------------------
+    
+   
+    // ------------------ gui settings -------------------------------
+    gui.setup();
+    gui.add(fade1.set("fade1",255, 0, 255));
+    gui.add(fade2.set("fade2",255, 0, 255));
+    gui.add(random.set("random",0.2, 0., 1.));
+    // ----------------------------------------------------------------
+    
+    
+    
+    
+    //settings temporales
+   // ofSetCircleResolution(50);
+  //  pos.set(860,206);
+
     
 }
 
@@ -34,59 +95,148 @@ void ofApp::setup(){
 void ofApp::update(){
     
     
+    int hifas_player_1 = micelio_player_1.size();
+    int hifas_player_2 = micelio_player_2.size();
+    int hifas_player_3 = micelio_player_3.size();
+  
+    for (int i=0;i<hifas_player_1;i++) {
+        
+        ofVec2f bump;
+        bump.set(ofRandom(-1,1),ofRandom(-1,1));
+        bump *= random;
+        bump += micelio_player_1[i]->getVelocity();
+        bump.normalize();
+        micelio_player_1[i]->setVelocity(bump.x,bump.y);
+        if (micelio_player_1.size()<80) { //reemplazar por hifas_player_1 ?????
+            if (ofRandom(0,1)<0.001) {
+                auto nueva = make_shared<CustomParticle>(box2d.getWorld(), micelio_player_1[i]->getPosition().x,micelio_player_1[i]->getPosition().y);
+                //micelio_player_1[i]->addAttractionPoint(0,1200,100);
+                micelio_player_1.push_back(nueva);
+            }
+        }
+    }
     
-    vel.x +=ofRandom(-1,1);
-    vel.y +=ofRandom(-1,1);
-    vel *=0.02;
-    vel.normalize();
+   for (int i=0;i<hifas_player_2;i++) {
+        
+        ofVec2f bump; // puedo usar bump ??? test!!
+        bump.set(ofRandom(-1,1),ofRandom(-1,1));
+        bump *= random;
+        bump += micelio_player_2[i]->getVelocity();
+        bump.normalize();
+        micelio_player_2[i]->setVelocity(bump.x,bump.y);
+ 
+    }
     
-    pos += vel;
+    for (int i=0;i<hifas_player_3;i++) {
+         
+         ofVec2f bump; // puedo usar bump ??? test!!
+         bump.set(ofRandom(-1,1),ofRandom(-1,1));
+         bump *= random;
+         bump += micelio_player_3[i]->getVelocity();
+         bump.normalize();
+         micelio_player_3[i]->setVelocity(bump.x,bump.y);
+  
+     }
     
-    fbo1.begin();
-    ofEnableAlphaBlending();
-    ofSetColor(255,255,255,255);
-    ofDrawCircle(pos.x,pos.y,1);
-    fbo1.end();
+    box2d.update();
+   
     
+    ofRemove(micelio_player_1, ofxBox2dBaseShape::shouldRemoveOffScreen);
+    ofRemove(micelio_player_2, ofxBox2dBaseShape::shouldRemoveOffScreen);
+    ofRemove(micelio_player_3, ofxBox2dBaseShape::shouldRemoveOffScreen);
+    
+    
+    fb_player_1.begin();
+    draw_fb_player_1();
+    fb_player_1.end();
 
+    fb_player_2.begin();
+    draw_fb_player_2();
+    fb_player_2.end();
+    
+   
+   
+
+    
 }
+
+void ofApp::draw_fb_player_1(){
+ 
+    for(auto &particle : micelio_player_1) {
+        particle->setRadius(0.3);
+        particle->draw();
+    }
+}
+
+void ofApp::draw_fb_player_2(){
+ 
+    for(auto &particle : micelio_player_2) {
+        particle->setRadius(1);
+        particle->draw();
+    }
+}
+
+
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
     float blur = ofMap(mouseX, 0, ofGetWidth(), 0, 2, true);
     
-    ofEnableAlphaBlending();
     ofSetColor(255);
-    
     fondo.draw(0,0,1920,1200);
+
+
+    
+    /*ofSetColor(255,255,255,fade1);
+    fb_player_1.draw(0,688);
+    ofSetColor(255,255,255,fade2);
+    fb_player_2.draw(0,688);
+    ofSetColor(255,255,255,255);
+    ofNoFill();
+    ofDrawCircle(mouseX,mouseY,10);*/
+
+
+  
+
+    fb_mix.begin();
+    fb_player_1.draw(0,0);
+    fb_player_2.draw(0,0);
+    fb_mix.end();
  
-    fbo2.begin();
+    //BLUR
+    
+    fb_blur_X.begin();
     ofEnableAlphaBlending();
     ofSetColor(255);
     shaderX.begin();
-    shaderX.setUniformTexture("tex0", fbo1.getTexture(),1);
+    shaderX.setUniformTexture("tex0", fb_mix.getTexture(),1);
+    shaderX.setUniformTexture("tex1", fb_mix.getTexture(),1);
     shaderX.setUniform1f("blurAmnt", blur);
     shaderX.setUniform1f("texwidth", 1920);
-    fbo1.draw(0,0);
+    fb_mix.draw(0,0);
     shaderX.end();
-    fbo2.end();
+    fb_blur_X.end();
     
-    fbo3.begin();
+    fb_blur_Y.begin();
     ofEnableAlphaBlending();
     ofSetColor(255);
     shaderY.begin();
-    shaderY.setUniformTexture("tex0", fbo2.getTexture(),1);
+    shaderY.setUniformTexture("tex0", fb_blur_X.getTexture(),1);
     shaderY.setUniform1f("blurAmnt", blur);
     shaderY.setUniform1f("texheight", 1512);
-    fbo2.draw(0,0);
+    fb_blur_Y.draw(0,0);
     shaderY.end();
-    fbo3.end();
+    fb_blur_Y.end();
    
     
-    fbo3.draw(0,688);
+    fb_blur_Y.draw(0,688); // 0,688
     
-    ofDrawBitmapString("blur: " + ofToString(blur),20,20);
+    
+    ofDrawBitmapString("blur: " + ofToString(blur),860,20);
+    ofDrawBitmapString("y: "+ ofToString(mouseY),860,40);
+    
+    gui.draw();
     
 }
 
@@ -97,7 +247,32 @@ void ofApp::exit(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
+    if(key == 'z') {
+        //ofSetBackgroundAuto(false);
+        for (int i=0;i<4;i++) {
+            auto particle = make_shared<CustomParticle>(box2d.getWorld(), mouseX ,mouseY-688); //
+            //particle->addAttractionPoint(0,0,10);
+            micelio_player_1.push_back(particle);
+            
+        }
+    }
+    
+    if(key == 'x') {
+        //ofSetBackgroundAuto(false);
+        for (int i=0;i<10;i++) {
+            auto particle = make_shared<CustomParticle>(box2d.getWorld(), mouseX ,mouseY-688);
+            micelio_player_2.push_back(particle);
+        }
+    }
+    
+    if(key == 'c') {
+        //ofSetBackgroundAuto(false);
+        for (int i=0;i<10;i++) {
+            auto particle = make_shared<CustomParticle>(box2d.getWorld(), mouseX ,mouseY-688);
+            micelio_player_3.push_back(particle);
+        }
+    }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,31 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxBox2d.h"
+#include "ofxGui.h"
+
+// ------------------------------------------------- a simple extended box2d circle
+class CustomParticle : public ofxBox2dCircle {
+public:
+    ofColor color;
+    
+    CustomParticle(b2World * world, float x, float y) {
+        setPhysics(1, 0, 10);
+        setup(world, x, y, 1);
+    }
+        
+    
+    void draw() {
+        float radius = getRadius();
+        
+        ofPushMatrix();
+        ofTranslate(getPosition());
+       // ofSetHexColor(0xFFFFFF);
+        ofNoFill();
+        ofDrawCircle(0, 0, radius);
+        ofPopMatrix();
+    }
+};
 
 class ofApp : public ofBaseApp{
 
@@ -23,14 +48,31 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo) override;
 		void gotMessage(ofMessage msg) override;
     
+    void draw_fb_player_1() ;
+    void draw_fb_player_2() ;
+    void draw_fb_player_3() ;
+    
     ofShader shaderX;
     ofShader shaderY;
-    
-    ofFbo fbo1;
-    ofFbo fbo2;
-    ofFbo fbo3;
     ofImage fondo;
+    ofxBox2d box2d;  // box2d world
     
-    ofVec2f pos;
-    ofVec2f vel;
+    vector    <shared_ptr<CustomParticle>> micelio_player_1; // this is a custom particle the extends a circle
+    vector    <shared_ptr<CustomParticle>> micelio_player_2;
+    vector    <shared_ptr<CustomParticle>> micelio_player_3;
+    
+    
+    ofFbo fb_player_1;
+    ofFbo fb_player_2;
+    ofFbo fb_player_3;
+    ofFbo fb_mix;
+    ofFbo fb_blur_X;
+    ofFbo fb_blur_Y;
+    
+    ofxPanel gui;
+    ofParameter<float> random;
+    ofParameter<int> fade1,fade2;
+    
+  
+    
 };
